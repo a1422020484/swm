3f77a469162162ba2bb0921a0fb29135127d323c
a1422020484 <1422020484@qq.com>
2016-09-19 16:11:23 +0800

    filter

---
+++ swm/src/swm/filter/HelloWorldFilter.java
@@ -0,0 +1,33 @@
+package swm.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorldFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy method");
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("doFilter method");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init method");
+	}
+
+}
