29267469a78047cc8a233e920ea1a3a2140b37c4
a1422020484 <1422020484@qq.com>
2016-09-23 16:42:15 +0800

    mybatis test

---
+++ swm/src/swm/mybatis/MybatisSingle.java
@@ -0,0 +1,24 @@
+package swm.mybatis;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.Connection;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+
+public class MybatisSingle {
+	public static void main(String[] args) throws IOException {
+		String resource = "/swm/config/mybatis/mybatis-single.xml";
+		Reader reader = Resources.getResourceAsReader(resource);
+		SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+		sqlMapper.openSession();
+		TransactionFactory transactionFactory = new  JdbcTransactionFactory();
+		Connection conn =  null;
+		transactionFactory.newTransaction(conn);
+		
+	}
+}
